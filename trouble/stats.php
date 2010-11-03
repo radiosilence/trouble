a02b82d2f6016808e43a98358f3d938bbf59e9fb
James Cleveland <jamescleveland@gmail.com>
2010-11-03 02:58:07 +0000

    new classes time..

---
+++ trouble/stats.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of trouble.
+ *
+ * (c) James Cleveland <jamescleveland@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Trouble;
+
+class Stats {
+    
+}
