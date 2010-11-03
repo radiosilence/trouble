cfafccd981f6e4a06b86771d61ceb0b961eea815
James Cleveland <jamescleveland@gmail.com>
2010-11-03 15:18:19 +0000

    iunno

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$cfg['database'] = array(
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'trouble',
+    'user' => 'trouble',
+    'password' => 'sZZ3FJRnXZKndQMu',
+);
+
+$cfg['crypto'] = array(
+    'keyphrase' => "the quick brown fox was taking meth",
+    'base_salt' => "the lazy dog lives off benefits"
+);
--- templates/killboard.php
+++ templates/killboard.php
@@ -8,14 +8,21 @@
       <th>Weapon</th>
     </thead>
     <tbody>
-  <? $game->kills->map(function($kill){ ?>
+<?php if($game->kills->count() > 0) { 
+    $game->kills->map(function($kill){ ?>
     <tr>
       <th scope="row"><?=$kill->when_happened?></td>
       <td ><a href="agent/<?=$kill->assassin->alias?>"><?=$kill->assassin->alias?></a></td>
       <td ><a href="agent/<?=$kill->target->alias?>"><?=$kill->target->alias?></a></td>
       <td ><a href="  weapon/<?=$kill->weapon->id?>" class="weapon"><?=$kill->weapon->name?></a></td>
     </tr>
-  <?php }); ?>
+  <?php }); 
+  } else { ?>
+    <tr>
+    <td colspan="4" class="message">
+      There have been no kills yet this game.
+    </td>
+  <?php } ?>
     </tbody>
   </table>
   </article>
