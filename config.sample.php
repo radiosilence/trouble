e65bbd54329309d580f971b32182e188641ee007
James Cleveland <jamescleveland@gmail.com>
2010-11-04 10:35:39 +0000

    made config ignored and added sample config

---
+++ config.sample.php
@@ -0,0 +1,14 @@
+<?php
+
+$cfg['database'] = array(
+    'driver' => '{DB_DRIVER}',
+    'host' => '{DB_HOST}',
+    'database' => '{DB_NAME}',
+    'user' => '{DB_DRIVER}',
+    'password' => '{DB_PASSWORD}',
+);
+
+$cfg['crypto'] = array(
+    'keyphrase' => "{KEYPHRASE}",
+    'base_salt' => "{SALT}"
+);
