98368daec9049a03418dd7213182d4baab352e7e
James Cleveland <jamescleveland@gmail.com>
2011-02-18 05:56:53 +0000

    symlinked static

--- templates/main.php
+++ templates/main.php
@@ -4,11 +4,11 @@
     <base href="http://<?=HOST?><?=BASE_HREF?>/">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 
-    <link rel="stylesheet" href="http://0xf.nl/css_lib/reset.css" type="text/css"/>
+    <link rel="stylesheet" href="css_lib/reset.css" type="text/css"/>
 
-    <link rel="stylesheet" href="http://0xf.nl/css_lib/grid.css" type="text/css"/>
-    <link rel="stylesheet" href="http://0xf.nl/css_lib/fileuploader.css" type="text/css">
-    <link rel="stylesheet" href="http://0xf.nl/css/subverse.css" type="text/css"/>
+    <link rel="stylesheet" href="css_lib/grid.css" type="text/css"/>
+    <link rel="stylesheet" href="css_lib/fileuploader.css" type="text/css">
+    <link rel="stylesheet" href="css_lib/subverse.css" type="text/css"/>
     <script src="js_lib/jquery.js" type="text/javascript"></script>
     <title><?=$title?> | Trouble</title>
   </head>
